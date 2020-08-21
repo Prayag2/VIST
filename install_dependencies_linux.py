745359d8bec9868761e3b47aceeb25f768860834
Aditya Tripathi <axityatrips@gmail.com>
2020-08-21 12:29:59 +0530

    changed linux installer

--- install_dependencies_linux.py
+++ install_dependencies_linux.py
@@ -1,18 +1,53 @@
 import requests
+from os import system
 from subprocess import call
 from sys import version_info
 
 
+def install_pip():
+    while True:
+        distro_id = int(input(
+            "Please select your linux distro (1. Ubuntu/Debian, 2. Arch, 3. Fedora, 4. OpenSUSE): ")) - 1
+        commands = [['sudo apt install python3-pip'], ['sudo pacman -S python-pip'],
+                    ['sudo yum install epel-release', 'sudo yum install python3-pip'], ['sudo zypper install python3-pip']]
+        try:
+            for command in commands[distro_id]:
+                c = command.split()
+                call(c)
+        except:
+            print("There was an error. Please try again.")
+            continue
+        else:
+            print("Pip Installed Successfully!")
+            break
+
+
 def install_modules():
     # Import modules
+    from os.path import isfile, join
+    from sys import prefix
     from pkgutil import iter_modules
+
+    # Check if pip is installed
+    pip_path = join(prefix, 'bin', 'pip3')
+    if not isfile(pip_path):
+        install_pip()
+        # Check again
+        if not isfile(pip_path):
+            # Raise error if couln't install pip
+            print("Failed to find or install pip! Please install it manually!")
+            quit()
 
     modules = ['SpeechRecognition', 'gTTS', 'pydub', 'requests', 'geocoder']
     for module in modules:
         if module not in [installed[1] for installed in iter_modules()]:
             print(f"Installing {module}...")
-            call(['pip3', 'install', module])
-            call(['pip', 'install', module])
+            try:
+                call(['pip3', 'install', module])
+                call(['pip', 'install', module])
+            except:
+                pass
+            system('clear')
             print(f"Installed {module} successfully!")
     print("All dependencies are installed!")
     quit()
